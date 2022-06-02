394f7b7a1da8af4e6fcd98a6cd1e10390dd8ec84
John G <105654452+jwg7@users.noreply.github.com>
2022-06-02 14:14:38 -0500

    Add files via upload

--- secondCharOfThirdString.js
+++ secondCharOfThirdString.js
@@ -4,7 +4,7 @@ let userArray = JSON.parse(prompt(`Enter an array: `));
 
 
 if (typeof userArray[2] === 'string') {
-console.log(userArray[3][2])
+console.log(userArray[2][2])
 } else {
     console.log(`Item 3 isn't a string. Error.`)
 }
